0111a4ef7112c28bf0ebb0e3874deb2351c103ec
Hasanuzzaman <shuvo009@yahoo.com>
2020-07-19 17:11:01 +0600

    in memory mmongoDb setup

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,7 +1,16 @@
 const request = require('supertest')
 const app = require('../server')
+const mongoose = require('mongoose');
 
 describe('Post Endpoints', () => {
+
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true }, () => {
+      console.log("aaaa");
+    });
+  });
+
+
   it('should create a new post', async () => {
     const res = await request(app)
       .get('/users')
