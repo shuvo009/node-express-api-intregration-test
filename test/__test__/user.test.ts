2630ed2632fb75d3e9040af44dbcf995790f314e
Hasanuzzaman <shuvo009@yahoo.com>
2020-07-19 19:30:39 +0600

    move test code at __test__ folder

--- test/__test__/user.test.ts
+++ test/__test__/user.test.ts
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../server')
+const app = require('../../src/server')
 const mongoose = require('mongoose');
 
 describe('Post Endpoints', () => {
