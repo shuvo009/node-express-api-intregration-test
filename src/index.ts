f712f8ddf23612a190544a05000b1a0fd66cdb98
Hasanuzzaman <shuvo009@yahoo.com>
2020-07-19 22:38:17 +0600

    missing package

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 const app = require("./server");
 import databaseConnect from "./dataLayer/databaseConnect";
 
-const dbConnectionString: string = "mongodb://dev_admin:159!QAZ@ds115768.mlab.com:15768/shopway_dev"
+const dbConnectionString: string = "mongodb://<username>:<password>@ds115768.mlab.com:15768/<database>"
 databaseConnect(dbConnectionString);
 app.listen(3000)
 console.log("Server Started at 3000")
