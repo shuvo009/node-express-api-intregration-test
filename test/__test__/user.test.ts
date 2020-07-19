0b1daf4a7ea8534ae40541d3d48deca73a1231b1
Hasanuzzaman <shuvo009@yahoo.com>
2020-07-19 21:59:48 +0600

    create user test

--- test/__test__/user.test.ts
+++ test/__test__/user.test.ts
@@ -3,7 +3,7 @@ import request from "supertest"
 import databaseConnect from "../../src/dataLayer/databaseConnect";
 import userSchema from "../../src/dataLayer/userSchema";
 
-describe('Get List of User', () => {
+describe('Get /users', () => {
 
   beforeAll(async () => {
     const connectionString = process.env.MONGO_URL;
@@ -25,5 +25,33 @@ describe('Get List of User', () => {
 
     expect(res.status).toEqual(200);
     expect(res.body.length).toEqual(2);
+  })
+})
+
+
+describe('Post /create', () => {
+
+  beforeAll(async () => {
+    const connectionString = process.env.MONGO_URL;
+    await databaseConnect(connectionString);
+  });
+
+  beforeEach(async () => {
+    await userSchema.remove({});
+  });
+
+
+  it('should create a user', async () => {
+    const data = { email: "email2@gmail.com", firstName: "user first name2", lastName: "user last name2" };
+    const res = await request(app)
+      .post('/create')
+      .send(data);
+
+    expect(res.status).toEqual(200);
+
+    const listRes = await request(app)
+      .get('/users');
+
+    expect(listRes.body.length).toEqual(1);
   })
 })
